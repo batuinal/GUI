24a5e0cb810c6bcbff871ea1012a3ee2e9aff8e2
Oliver Tang <oqt@umich.edu>
2014-10-24 00:36:03 -0400

    Adding login, songSelect, gameplay
    Adding windows for user login, song selection, and main gameplay window
    frame.

---
+++ ara/src/ara/Home.java
@@ -0,0 +1,19 @@
+package ara;
+
+import java.awt.Color;
+import java.awt.FlowLayout; 
+
+import javax.swing.JFrame; 
+
+public class Home 
+{
+	public static void main (String []args)
+	{
+		// Open up HomeFrame (user sign in frame)
+		HomeFrame win; 
+		win = new HomeFrame ("Welcome to Beats with Ara!"); 
+		win.setSize(300, 350); 
+		win.setVisible(true); 
+		win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+	}
+}
---
+++ ara/src/ara/gameFrame.java
@@ -0,0 +1,114 @@
+package ara;
+
+import java.awt.event.ActionEvent; 
+import java.awt.event.ActionListener; 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout; 
+
+import javax.swing.JLabel;
+import javax.swing.JFrame;
+import javax.swing.JButton; 
+import javax.swing.ImageIcon; 
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import ara.HomeFrame.HomeListener;
+import ara.SongSelectFrame.SongSelectListener;
+import static java.lang.System.out; 
+
+
+public class gameFrame extends JFrame
+{
+	// Variable Declaration
+	private JLabel headerLabel; 
+	private JLabel ScoreLabel;
+	private JButton pauseButton; 
+	private JButton resetButton; 
+	private gameListener buttonListener; 
+	
+	public gameFrame (String inTitle)
+	{
+		// Layout Panels
+		JPanel headerPanel; 
+		JPanel gameplayPanel; 
+		JPanel buttonPanel; 
+		
+		// Header
+		String headerMessage = "Current Song: " + inTitle; 
+		headerPanel = new JPanel (new FlowLayout()); 
+		headerPanel.add (new JLabel (headerMessage, SwingConstants.CENTER));
+		
+		// Main gameplay content
+		gameplayPanel = new JPanel (new FlowLayout()); 
+		gameplayPanel.add (new JLabel ("Touch corresponding buttons on the knit wall!", SwingConstants.CENTER)); 
+		
+		// Bottom Buttons 
+		buttonPanel = new JPanel (new FlowLayout()); 
+		pauseButton = new JButton("Pause"); 
+		resetButton = new JButton ("Reset"); 		
+		buttonPanel.add (pauseButton);
+		buttonPanel.add (resetButton); 
+		
+		// Set GUI Layout
+		setLayout(new BorderLayout()); 
+		add(headerPanel, BorderLayout.NORTH);
+		add(gameplayPanel, BorderLayout.CENTER);
+		add(buttonPanel, BorderLayout.SOUTH);
+		
+		// Button Listeners
+		buttonListener = new gameListener(); 
+		pauseButton.addActionListener(buttonListener); 
+		resetButton.addActionListener(buttonListener); 	
+	}
+	
+	public class gameListener implements ActionListener
+	{
+		public void actionPerformed (ActionEvent e)
+		{
+			if (e.getSource() == pauseButton){
+				out.println("Pause the game!"); 
+			}
+			else if (e.getSource() == resetButton){
+				out.println("Reset the game!"); 
+			}
+		}
+	} // end of gameListener	
+} // end of gameFrame
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
