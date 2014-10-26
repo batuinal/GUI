cd4d7581ecd57406048f32ddf34c45bb63cc39c9
Ruifu Zhang <zhangrf1992@hotmail.com>
2014-10-26 15:02:16 -0400

    Javadatabse connection complete

--- ara/src/ara/database.java
+++ ara/src/ara/database.java
@@ -1,6 +1,52 @@
 package ara;
 
-public class database
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Database
 {
-  public
+  private Connection con;
+  public void connect() throws Exception
+  {
+    if(con != null) return;
+    
+    try
+    {
+      Class.forName("com.mysql.jdbc.Driver");
+    }
+    catch( ClassNotFoundException e )
+    {
+      throw new Exception("Driver Not Found");
+    }
+
+    String connectionUrl = "jdbc:mysql://localhost:3306/gamedb";
+    String user = "root";
+    String password = "group4";
+
+    try{
+      con = DriverManager.getConnection(connectionUrl, user, password);
+    }catch(SQLException e ){
+      System.out.println("Connection Failed!");
+      return;
+    }
+    
+    System.out.println("Connected: " + con);
+  }
+
+  public void disconnect()
+  {
+    if(con != null)
+    {
+      try
+      {
+        con.close();
+      }
+      catch( SQLException e )
+      {
+        System.out.println("Can't close connection");
+      }
+    }
+  }
+
 }
--- ara/src/ara/testJDBC.java
+++ ara/src/ara/testJDBC.java
@@ -5,5 +5,17 @@ public class testJDBC
   public static void main(String[] argc)
   {
     System.out.println("Test JavaDatabse");
+    
+    
+    Database db = new Database(); 
+    try{
+      db.connect();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    db.disconnect();
   }
+  
+
+  
 }
